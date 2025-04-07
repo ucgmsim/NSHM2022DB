a55e0c824f5cc7d06241b47cf5ec7389ab9dad36
Jake Faulkner <jakefaulkn@gmail.com>
2025-04-08 10:30:14 +1200

    test: add full run test of the NSHMDB

--- tests/test_nshmdb_generator.py
+++ tests/test_nshmdb_generator.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from nshmdb.nshmdb import NSHMDB
 from nshmdb.scripts import nshm_db_generator
 
-CRU_FAULT_SOLUTIONS = Path("tests") / "CRU_fault_system_solution.zip"
+CRU_FAULT_SOLUTIONS = Path("tests") / "CRU_fault_system_solution_small.zip"
 
 
 def test_nshmdb_generator(tmp_path: Path):
@@ -17,3 +17,13 @@ def test_nshmdb_generator(tmp_path: Path):
     assert rupture.area == 1090332700
     assert rupture.length == 34817.69
     assert set(rupture.faults) == {"Acton"}
+
+
+CRU_FAULT_SOLUTIONS_FULL = Path("tests") / "CRU_fault_system_solution.zip"
+
+
+def test_nshmdb_generator_runs(tmp_path: Path):
+    nshmdb_path = tmp_path / "nhsmdb.db"
+    nshm_db_generator.main(CRU_FAULT_SOLUTIONS_FULL, nshmdb_path)
+    db = NSHMDB(nshmdb_path)
+    assert db is not None
